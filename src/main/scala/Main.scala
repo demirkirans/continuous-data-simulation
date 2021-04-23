d52e0a178318e61967b743121d8bdeec46733cf5
demirkirans <serhat.demirkiran@gmail.com>
2021-04-23 03:02:31 +0300

    Calculation added for part ii

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,23 +3,28 @@ import com.sksamuel.elastic4s.http.JavaClient
 import com.sksamuel.elastic4s.{ElasticClient, ElasticProperties}
 import com.sksamuel.elastic4s.requests.common.RefreshPolicy
 import com.sksamuel.elastic4s.requests.searches.SearchResponse
-
+import scala.util.control.Breaks._
 
 
 object PredictionIndex extends App {
 
   // in this example we create a client to a local Docker container at localhost:9200
-  val props = ElasticProperties("http://localhost:9200")
-  val client = ElasticClient(JavaClient(props))
+  //val props = ElasticProperties("http://localhost:9200")
+  //val client = ElasticClient(JavaClient(props))
 
   // we must import the dsl
   import com.sksamuel.elastic4s.ElasticDsl._
+  
+  //////////////////////////////////////////////PART 1/////////////////////////////////////////////////////
+
 
   //get current directory and add relative paths
   val dir = os.pwd / "data" / "tazi-se-interview-project-data.csv"
 
   //keep time counter
   val start = System.nanoTime
+
+  
 
   //read and process csv file
   val bufferedSource = io.Source.fromFile(dir.toString)
@@ -42,77 +47,156 @@ object PredictionIndex extends App {
     }
     bufferedSource.close
 
-  //data population is completed
-  val difference = (System.nanoTime - start) / 1e9d
-
-  println("Time: " + difference + " second")
-  /*
   
+
+  //////////////////////////////// PART 2    ////////////////////////////////////////////////////////////
+
+  def calculateProb(model1: Double, model2: Double, model3:Double): Double = {
+    //weights for models
+    val weight1 = 0.5
+    val weight2 = 0.6
+    val weight3 = 0.7
+
+    return (model1*weight1 + model2*weight2 + model3*weight3) / 3
+  }
+
+  def findConfusion(predictions_A: Array[Double], predictions_B: Array[Double], given_label: String): String = {
+    /*
+    get predictions for A and B label,
+    returns confusion(TP, FN, FP or TN)
+    */
+    var weightedAverage_A: Double = calculateProb(predictions_A(0), predictions_A(1), predictions_A(2))
+    var weightedAverage_B: Double = calculateProb(predictions_B(0), predictions_B(1), predictions_B(2))
+    //compare them and get predicted label
+    var predicted_label: String = ""
+    if (weightedAverage_A > weightedAverage_B) { predicted_label = "A" } else { predicted_label = "B" }
+    //conclusion
+    var confusion: String = ""
+    if (given_label == "A" && predicted_label == "A")      { confusion = "TP" }
+    else if (given_label == "A" && predicted_label == "B") { confusion = "FN" }
+    else if (given_label == "B" && predicted_label == "A") { confusion = "FP" }
+    if (given_label == "B" && predicted_label == "B")      { confusion = "TN" }
+
+    return confusion
+
+  }
 
   val props = ElasticProperties("http://localhost:9200")
   val client = ElasticClient(JavaClient(props))
 
-  // we must import the dsl
-  import com.sksamuel.elastic4s.ElasticDsl._
-
-  // Next we create an index in advance ready to receive documents.
-  // await is a helper method to make this operation synchronous instead of async
-  // You would normally avoid doing this in a real program as it will block
-  // the calling thread but is useful when testing
-  
-  
+  /*
   client.execute {
-    createIndex("artists").mapping(
+    createIndex("confusion_matrix")mappings(
       properties(
-        TextField("name")
+        IntegerField("True Positive"),
+        IntegerField("False Negative"),
+        IntegerField("False Positive"),
+        IntegerField("True Negative")
+
+        //"True Positive" typed IntegerType,
+        //"False Negative" typed IntegerType,
+        //"False Positive" typed IntegerType,
+        //"True Negative" typed IntegerType
       )
     )
   }.await
-  
-  val start = System.nanoTime
-  // Next we index a single document which is just the name of an Artist.
-  // The RefreshPolicy.Immediate means that we want this document to flush to the disk immediately.
-  // see the section on Eventual Consistency.
-  client.execute {
-    indexInto("customer").fields("name" -> "Muso", "surname" -> "Demirkiran").refresh(RefreshPolicy.Immediate)
-  }.await
-
-  
-
-  // now we can search for the document we just indexed
-
-  
-
-  val resp = client.execute {
-    search("artists").query("lowry")
-  }.await
-
-  
-
-  // resp is a Response[+U] ADT consisting of either a RequestFailure containing the
-  // Elasticsearch error details, or a RequestSuccess[U] that depends on the type of request.
-  // In this case it is a RequestSuccess[SearchResponse]
-
-  
-
-  println("---- Search Results ----")
-  resp match {
-    case failure: RequestFailure => println("We failed " + failure.error)
-    case results: RequestSuccess[SearchResponse] => println(results.result.hits.hits.toList)
-    case results: RequestSuccess[_] => println(results.result)
-  }
-
-  
-
-  // Response also supports familiar combinators like map / flatMap / foreach:
-  //resp foreach (search => println(s"There were ${search.totalHits} total hits"))
-
-  val finish = (System.nanoTime - start) / 1e9d
-
   */
 
-  client.close()
 
+  //hash map for confusion matrix
+  import scala.collection.mutable.HashMap
+
+  val confusionMatrix: HashMap[String, Int] = HashMap("TP"->0,"FN"->0,"FP"->0,"TN"->0)
+
+  //queue for sliding window
+  import scala.collection.mutable.Queue
+
+  val slidingWindow = Queue[String]()
+
+  val bufferedSource = io.Source.fromFile(dir.toString)
+    var window: Int                = 1
+    var after1000instance: Boolean = false
+    var headOfWindow:String        = ""
+    var confusion: String          = ""
+
+    for (line <- bufferedSource.getLines.drop(1)) {
+        val cols = line.split(",").map(_.trim)
+
+        if (after1000instance) {
+          //remove head of window
+          //together with its confusion
+          headOfWindow = slidingWindow.dequeue
+          confusionMatrix(headOfWindow) -= 1
+          //calculate new instance
+          //and add to queue
+
+          confusion = findConfusion(
+            predictions_A = Array(cols(2).toDouble, cols(4).toDouble, cols(6).toDouble), 
+            predictions_B =  Array(cols(3).toDouble, cols(5).toDouble, cols(7).toDouble),
+            given_label = cols(1)
+          )
+
+          confusionMatrix(confusion) += 1
+
+          //add confusion to the queue
+          slidingWindow.enqueue(confusion)
+
+          //lastly index the confusion matrix
+          client.execute {
+            indexInto("confusion_matrix").id(window.toString).fields(
+              "True Positive"  -> confusionMatrix("TP"),
+              "False Negative" -> confusionMatrix("FN"),
+              "False Positive" -> confusionMatrix("FP"),
+              "True Negative"  -> confusionMatrix("TN")
+            ).refresh(RefreshPolicy.Immediate)
+          }.await
+
+          //////////////////////////////////
+          window += 1
+          println("SLIDING WINDOW: " + window)
+        }
+        
+
+        confusion = findConfusion(
+          predictions_A = Array(cols(2).toDouble, cols(4).toDouble, cols(6).toDouble), 
+          predictions_B =  Array(cols(3).toDouble, cols(5).toDouble, cols(7).toDouble),
+          given_label = cols(1)
+        )
+
+        confusionMatrix(confusion) += 1
+
+        //add confusion to the queue
+        slidingWindow.enqueue(confusion)
+
+        //if sliding windows reaches size of 1000
+
+        if (cols(0).toInt == 1000) {
+          //First window
+          
+          //Index confusion matrix to ElasticSearch
+
+          client.execute {
+            indexInto("confusion_matrix").id(window.toString).fields(
+              "True Positive"  -> confusionMatrix("TP"),
+              "False Negative" -> confusionMatrix("FN"),
+              "False Positive" -> confusionMatrix("FP"),
+              "True Negative"  -> confusionMatrix("TN")
+            ).refresh(RefreshPolicy.Immediate)
+          }.await
+
+          window += 1
+          after1000instance = true
+        }
+
+        
+    }
+    bufferedSource.close
+
+  //data population is completed
+  val difference = (System.nanoTime - start) / 1e9d
+
+  println("Time: " + difference + " second")
   
-  
+
+  client.close()  
 }
